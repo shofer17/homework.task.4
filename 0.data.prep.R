7846df8c8456070bd31056bcad2c83d2127c6cae
Silvan Hofer <sm.hofer@bluewin.ch>
2021-04-04 11:02:31 +0200

    Start work

--- 0.data.prep.R
+++ 0.data.prep.R
@@ -1,2 +1,18 @@
-print("Hello World")
-print("Hi There!")
+#Query: Homwork task 4
+#Supervisor: Piotr
+#Date: 4.4.2021 
+#This Document contains the data prep for the Homework task 4
+
+rm(list = ls())
+
+#install.packages("openxlsx")
+library(openxlsx)
+library(readxl)
+library(gtalibrary)
+library(tidyr)
+library(dplyr)
+gta_setwd()
+
+
+
+gtalibrary::gta_data_slicer()
