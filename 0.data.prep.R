e9013d7072fe09df63764fdf6386e418bf0d192f
Silvan Hofer <sm.hofer@bluewin.ch>
2021-04-04 11:22:45 +0200

    add dates

--- 0.data.prep.R
+++ 0.data.prep.R
@@ -15,11 +15,12 @@ gta_setwd()
 
 
 countries = gtalibrary::country.names
+dates = as.Date(c("2009-12-31", "2012-12-31", "2015-12-31", "2018-12-31"))
 
 g20.plus.eu = countries %>% filter((is.g20 == TRUE) | (is.eu == TRUE)) %>% 
   select(c("name", "un_code", "is.eu"))
 
 eu = filter(countries, is.eu == TRUE)
 
-
-gtalibrary::gta_data_slicer(implementing.country = c()
+for (date in dates){
+gtalibrary::gta_data_slicer(implementing.country = g20.plus.eu$name, affected.country = g20.plus.eu, implementation.period = 
